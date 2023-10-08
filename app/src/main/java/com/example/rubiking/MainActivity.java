39d7f4107cccfba030758e71ca1aead77d2bf64f
sunnyapatel267 <119470303+sunnyapatel267@users.noreply.github.com>
2023-10-08 10:23:47 -0400

    Added to Activty_main and created task_layout.

--- app/src/main/java/com/example/rubiking/MainActivity.java
+++ app/src/main/java/com/example/rubiking/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rubiking;
 
 import android.os.Bundle;
 
+import com.example.rubiking.Adapter.ToDoAdapter;
+import com.example.rubiking.Model.ToDoModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,46 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rubiking.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private List<ToDoModel> taskList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a Test Task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
